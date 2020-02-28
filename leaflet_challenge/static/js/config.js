691bbd946f09b4f7fe8e3e48ee8963cca16fdbd7
JimCDabc <jim.comas@gmail.com>
2020-02-28 15:19:02 -0600

    added my limited github.io token that will only work from JimcDABC.github.io

---
+++ leaflet_challenge/static/js/config.js
@@ -0,0 +1,5 @@
+// API key
+// const API_KEY = "YOUR KEY HERE"
+
+// my github.io token for Mapbox
+const API_KEY = pk.eyJ1IjoiamNvbWFzIiwiYSI6ImNrNzZvZng5NDBlOHkzbHNkenVuNWxlaTQifQ.9QjiywT5DXq8XDkQbiXdOw
